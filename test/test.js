520d6aaf23fe2fe3452f7e95efc87f615d27220b
Chris Callahan <callahan.c@gmail.com>
2015-09-24 18:38:14 -0400

    add integration test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
 import assert from 'assert';
 import React from 'react';
+import { findDOMNode } from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import PaperCheckbox from '../src/paper-checkbox';
+import { jsdom } from 'jsdom';
+
 
 describe('PaperCheckbox', () => {
 
@@ -50,6 +53,49 @@ describe('PaperCheckbox', () => {
       let result = shallowRenderer.getRenderOutput();
       let checkbox = result.props.children;
       assert(checkbox.props.className.match(/checked/));
+    });
+
+  });
+
+
+  describe('behavior', () => {
+    let Container = class extends React.Component {
+      constructor(props, context) {
+        super(props, context);
+        this.state = { checked: false };
+      }
+
+      render() {
+        return (
+          <div>
+            <PaperCheckbox
+              ref='checkbox'
+              checked={this.state.checked}
+              onClick={() => this.setState({ checked: !this.state.checked })}
+            />
+          </div>
+        );
+      }
+    };
+
+    before(() => {
+      global.document = jsdom('<!doctype html><html><body></body></html>');
+      global.window = global.document.defaultView;
+    });
+
+    it('calls an onClick callback when clicked', () => {
+      let instance = TestUtils.renderIntoDocument(<Container />);
+      let container = TestUtils.findRenderedComponentWithType(instance, Container);
+      let reactCheckbox = TestUtils.findRenderedComponentWithType(instance, PaperCheckbox);
+      let domCheckbox = findDOMNode(container.refs.checkbox);
+
+      assert.equal(reactCheckbox.props.checked, false);
+
+			TestUtils.Simulate.click(domCheckbox);
+      assert.equal(reactCheckbox.props.checked, true);
+
+			TestUtils.Simulate.click(domCheckbox);
+      assert.equal(reactCheckbox.props.checked, false);
     });
 
   });
